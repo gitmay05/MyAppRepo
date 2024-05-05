df339e52b89f78097288dfc7976dd87fd6d746f8
sandeepsk3494@gmail.com <sandeepsk3494@gmail.com>
2024-05-05 21:25:29 +0530

    changes

---
+++ AllinOneApiApplication/Interface/ApplicationUser/IApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using AllinOneApiApplication.ModelClass.UserModel;
+using AllinOneApiApplication.ModelClass.UserModel;
+
+namespace AllinOneApiApplication.Interface.ApplicationUser
+{
+    public interface IApplicationUser
+    {
+        List<ApplicationUser> UserDetails { get; set; }
+    }
+}
---
+++ AllinOneApiApplication/ModelClass/UserModel/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿namespace AllinOneApiApplication.ModelClass.UserModel
+{
+    public class ApplicationUser
+    {
+        public string UserName { get; set; }
+    }
+}
