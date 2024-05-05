77b26dfba270dd44208f266da679d6a43f3139e2
sandeepsk3494@gmail.com <sandeepsk3494@gmail.com>
2024-05-05 22:26:01 +0530

    user repo checkin

--- AllinOneApiApplication/Repository/User/UserReposistory.cs
+++ AllinOneApiApplication/Repository/User/UserReposistory.cs
@@ -1,16 +1,69 @@
-﻿using AllinOneApiApplication.Interface.ApplicationUser;
+﻿using AllinOneApiApplication.CommonDataAccess;
+using AllinOneApiApplication.Interface.ApplicationUser;
 using AllinOneApiApplication.Model.UserModel;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace AllinOneApiApplication.Repository.User
 {
     public class UserReposistory : IApplicationUser
     {
+        #region
+        static DataFunctions objDataFunctions = null;
+        System.Data.DataSet objDataSet = null;
+        List<user> objuserList = null;
+        static string _commandText = string.Empty;
+        #endregion
+        public UserReposistory()
+        {
+            objDataFunctions = new DataFunctions();
+        }
+
         public List<user> UserDetails()
         { 
-        List<user> Details = new List<user>();
-        user objuser = new user() { UserName = "Prince", UserPwd = "pwd" };
-        Details.Add(objuser);
-        return Details;
+ 
+            var result = objuserList;
+            objuserList = new List<user>();
+
+            System.Data.DataSet objDataSet = null;
+            try
+            {
+                List<SqlParameter> parms = new List<SqlParameter>()
+                {
+
+                    new SqlParameter("@id","1"),
+                    
+                };
+
+                _commandText = "[dbo].[USP_yourproc]";
+                objDataSet = (DataSet)objDataFunctions.getQueryResult(_commandText, DataReturnType.DataSet, parms);
+                if (objDataSet.Tables[0].Rows.Count > 0)
+                {
+
+                    if (objDataSet.Tables[0].Rows[0].Field<int>("Message_Id") == 1)
+                    {
+                        objuserList = objDataSet.Tables[1].AsEnumerable().Select(dr => new user()
+                        {
+                            UserName = dr.Field<string>("CaseID"),
+                            UserPwd = dr.Field<string>("RatingCount"),
+                          
+                        }).ToList();
+                        objDataSet.Dispose();
+                        result = objuserList;
+                    }
+                    else
+                    {
+                        result = null;
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                result = null;
+            }
+            return result;
+
         }
     }
 }
